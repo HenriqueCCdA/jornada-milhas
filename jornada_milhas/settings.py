fdc45481f833d171a3b670301aa37fc751ee213c
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-07-19 18:43:26 -0300

    CONFIG(3): Configura os arquivos de media.
    close  #3

--- jornada_milhas/settings.py
+++ jornada_milhas/settings.py
@@ -102,15 +102,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles/"
 
-STATIC_URL = "static/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles/"
 
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 
 DEFAULT_RENDERER_CLASSES = ["rest_framework.renderers.JSONRenderer"]
 
